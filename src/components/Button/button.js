5ff17a9cfbdcef633676b94e63c289788f54088e
liappi <li.vilia039@gmail.com>
2018-08-18 16:03:29 +1200

    changed dropdown to form with submit button

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -33,49 +33,44 @@ export default class Button extends Component {
           onClick={() => { this.handleClick() }}>
           {this.state.label}
         </button> */}
-        <div class="dropdown">
-          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Choose region
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Auckland</a>
-            <a class="dropdown-item" href="#">Bay of Plenty</a>
-            <a class="dropdown-item" href="#">Canterbury</a>
-            <a class="dropdown-item" href="#">Hawke's Bay</a>
-            <a class="dropdown-item" href="#">Manawatu-Wanganui</a>
-            <a class="dropdown-item" href="#">Marlborough</a>
-            <a class="dropdown-item" href="#">Nelson</a>
-            <a class="dropdown-item" href="#">Northland</a>
-            <a class="dropdown-item" href="#">Otago</a>
-            <a class="dropdown-item" href="#">Southland</a>
-            <a class="dropdown-item" href="#">Taranaki</a>
-            <a class="dropdown-item" href="#">Tasman</a>
-            <a class="dropdown-item" href="#">Waikato</a>
-            <a class="dropdown-item" href="#">Wellington</a>
-            <a class="dropdown-item" href="#">West Coast</a>
+        <form>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Choose region</label>
+            <select class="form-control" id="regionSelect">
+              <option>Auckland</option>
+              <option>Bay of Plenty</option>
+              <option>Canterbury</option>
+              <option>Hawke's Bay</option>
+              <option>Manawatu-Wanganui</option>
+              <option>Marlborough</option>
+              <option>Nelson</option>
+              <option>Northland</option>
+              <option>Otago</option>
+              <option>Southland</option>
+              <option>Taranaki</option>
+              <option>Tasman</option>
+              <option>Waikato</option>
+              <option>Wellington</option>
+              <option>West Coast</option>
+            </select>
           </div>
-        </div>
-        <div class="dropdown">
-          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Type
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Beach</a>
-            <a class="dropdown-item" href="#">River</a>
-            <a class="dropdown-item" href="#">Lake</a>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Type</label>
+            <select class="form-control" id="type">
+              <option>Beach</option>
+              <option>River</option>
+              <option>Lake</option>
+            </select>
           </div>
-        </div>
-        <div class="dropdown">
-          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Rating
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Green</a>
-            <a class="dropdown-item" href="#">Amber</a>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Rating</label>
+            <select class="form-control" id="rating">
+              <option>Green</option>
+              <option>Amber</option>
+            </select>
           </div>
-        </div>
-        <button class="btn btn-primary" type="submit">Apply</button>
-
+          <button type="submit" class="btn btn-primary mb-2">Apply</button>
+        </form>
       </div>
     );
   }
