b691af626c157b3fb1bdc95477078f402045989a
Aastha Goyal <aastha2150@gmail.com>
2018-08-18 15:36:44 +1200

    Add Json data

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,19 @@ import ButtonWrapper from './components/Button'
 import TablePage from './containers/TablePage'
 import {Route} from 'react-router-dom';
 
+fetch("./json.json")
+
+.then(function(response) {
+    return response.json();
+})
+  .then(function(myJson) {
+      console.log(myJson);
+  });
+
+
+
+
+
 //This is sort of like the main method for our react project
 const App = () => {
   return (
