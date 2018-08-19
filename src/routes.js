4108f4ac0cd2f27159f31339987e7735d093ac3e
liappi <li.vilia039@gmail.com>
2018-08-19 16:21:52 +1200

    header links back to home page

--- src/routes.js
+++ src/routes.js
@@ -4,33 +4,36 @@ import ButtonWrapper from './components/Button'
 import TablePage from './containers/TablePage'
 import WaterQualityPage from './components/Quality'
 import MapPage from './components/MapPage'
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 //This is sort of like the main method for our react project
 const App = () => {
-  return ( 
-    <div className ='App'>
-          <header>
-            <img src="icon.jpg" class="float-left" id="icon"></img>
-            <img src="wanderer.jpg" height="105px"></img>
-          </header>
-        <div id="main">
+  return (
+    <div className='App'>
+      <Link to={`/`}>
+        <header>
+          <img src="icon.jpg" class="float-left" id="icon"></img>
+          <img src="wanderer.jpg" height="105px"></img>
+        </header>
+      </Link>
+      <div id="main">
         <main className='App-intro'>
-        <Route exact path='/'  component={ ButtonWrapper } />
-        <Route exact path='/table' component={TablePage} />
+          <Route exact path='/' component={ButtonWrapper} />
+          <Route exact path='/table' component={TablePage} />
 
-        <Route exact path='/WaterQualityPage' component={WaterQualityPage} />
-        <Route exact path='/map' component={MapPage} />
-      </main>
-      
+          <Route exact path='/WaterQualityPage' component={WaterQualityPage} />
+          <Route exact path='/map' component={MapPage} />
+        </main>
+
       </div>
-       <footer>
+      <footer>
         <p id="footer">DEVELOPED BY TEAM ROCKET @ 2018 <i class="far fa-thumbs-up" id="thumbup"></i></p>
-       </footer>
-      
-      </div>
-    
+      </footer>
+
+    </div>
+
   );
 }
 
